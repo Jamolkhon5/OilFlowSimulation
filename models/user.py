28c92564201b3d508dac928e78c3b96042b619d0
Jamolkhon5 <soliev.jamol@bk.ru>
2025-03-16 04:15:36 +0300

    Allow duplicate usernames while preserving email uniqueness

--- models/user.py
+++ models/user.py
@@ -13,7 +13,7 @@ class User(UserMixin, db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), nullable=False, index=True)
+    username = db.Column(db.String(64), unique=False, nullable=False, index=True)
     email = db.Column(db.String(120), unique=True, nullable=False, index=True)
     password_hash = db.Column(db.String(128), nullable=False)
     company = db.Column(db.String(120))
